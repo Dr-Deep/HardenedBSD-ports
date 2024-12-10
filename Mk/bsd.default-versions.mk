f3a9744bb664ef3a2c95d07932255df6f7d25311
Shawn Webb <shawn.webb@hardenedbsd.org>
2024-12-10 01:46:30 +0000

    HBSD: llvm 19 is now in 14-STABLE base, make it default
    14-STABLE kernel 1402501 includes llvm 19 in base. We still need llvm 18
    as the default for 1402500 and lower.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -90,7 +90,7 @@ LIBRSVG2_DEFAULT?=	legacy
 # Possible values: c7 rl9
 LINUX_DEFAULT?=		c7
 # Possible values: 11, 12, 13, 14, 15, 16, 17, 18, 19, -devel (to be used when non-base compiler is required)
-.if ${OSVERSION} >= 1500025
+.if ${OSVERSION} >= 1500025 || (${OSVERSION} >= 1400000 && ${OSVERSION} <= 1402500)
 LLVM_DEFAULT?=		19
 .else
 LLVM_DEFAULT?=		18
