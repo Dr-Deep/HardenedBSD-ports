adcbb22d38faa5dc2c7e1ca3a49e39987c342637
Shawn Webb <shawn.webb@hardenedbsd.org>
2025-11-10 20:49:12 +0000

    HBSD: ftp/curl: Fixup .onion patch
    We want people to still be able to reach HardenedBSD's infrastructure
    over Tor, so we need to fixup the patch that removes the prohibition of
    Tor Onion Services due to changes in libcurl's lib/hostip.c file.
    
    This commit should serve as a reminder of how changes we humans make to
    technology impact human rights efforts.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- ftp/curl/files/patch-lib_hostip.c
+++ ftp/curl/files/patch-lib_hostip.c
@@ -1,9 +1,12 @@
---- lib/hostip.c.orig	2025-06-02 14:46:26 UTC
+--- lib/hostip.c.orig	2025-11-10 20:46:52 UTC
 +++ lib/hostip.c
-@@ -822,12 +822,6 @@ CURLcode Curl_resolv(struct Curl_easy *data,
+@@ -858,15 +858,8 @@ CURLcode Curl_resolv(struct Curl_easy *data,
+   if(!dnscache)
+     goto error;
  
-   /* We should intentionally error and not resolve .onion TLDs */
+-  /* We should intentionally error and not resolve .onion TLDs */
    hostname_len = strlen(hostname);
+   DEBUGASSERT(hostname_len);
 -  if(hostname_len >= 7 &&
 -     (curl_strequal(&hostname[hostname_len - 6], ".onion") ||
 -      curl_strequal(&hostname[hostname_len - 7], ".onion."))) {
