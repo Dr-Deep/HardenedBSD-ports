d0b6658fe3bc133bcec1faf3d2fadc7caac64cc4
Shawn Webb <shawn.webb@hardenedbsd.org>
2026-01-21 18:15:45 -0700

    HBSD: Resolve merge conflict
    This port was updated upstream. We no longer need to carry a patch
    downstream to support our HARDCFLAGS option.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- archivers/zip/files/patch-zip.c
+++ archivers/zip/files/patch-zip.c
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
---- zip.c.orig	2025-11-20 14:51:53 UTC
-+++ zip.c
-@@ -1028,8 +1028,7 @@ local void help_extended()
-=======
 From: Santiago Vila <sanvila@debian.org>
 Subject: Use format specifier %s to print strings, not the string itself
 Bug-Debian: https://bugs.debian.org/673476
@@ -20,36 +15,19 @@ The additional space required in the `cmd` buffer is 4, not 3.
 --- zip.c.orig	2008-07-05 16:34:06 UTC
 +++ zip.c
 @@ -1028,7 +1028,7 @@ local void help_extended()
->>>>>>> internal/freebsd/main
  
    for (i = 0; i < sizeof(text)/sizeof(char *); i++)
    {
 -    printf(text[i]);
-<<<<<<< HEAD
--    putchar('\n');
-+    puts(text[i]);
-   }
- #ifdef DOS
-   check_for_windows("Zip");
-@@ -1225,8 +1224,7 @@ local void version_info()
-=======
 +    printf("%s", text[i]);
      putchar('\n');
    }
  #ifdef DOS
 @@ -1225,7 +1225,7 @@ local void version_info()
->>>>>>> internal/freebsd/main
              CR_MAJORVER, CR_MINORVER, CR_BETA_VER, CR_VERSION_DATE);
    for (i = 0; i < sizeof(cryptnote)/sizeof(char *); i++)
    {
 -    printf(cryptnote[i]);
-<<<<<<< HEAD
--    putchar('\n');
-+    puts(cryptnote[i]);
-   }
-   ++i;  /* crypt support means there IS at least one compilation option */
- #endif /* CRYPT */
-=======
 +    printf("%s", cryptnote[i]);
      putchar('\n');
    }
@@ -63,4 +41,3 @@ The additional space required in the `cmd` buffer is 4, not 3.
        ziperr(ZE_MEM, "building command string for testing archive");
      }
  
->>>>>>> internal/freebsd/main
